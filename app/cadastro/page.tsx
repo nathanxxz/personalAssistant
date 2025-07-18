a9073eddd1ed63d6259b677a1c7808f5bb72ca01
Fabricio Nathan <fabricionathan749@gmail.com>
2025-07-18 08:48:56 -0300

    Login e cadastro concluido

--- app/cadastro/page.tsx
+++ app/cadastro/page.tsx
@@ -1,29 +1,29 @@
-import { Label } from "@/components/ui/label";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { Label } from "@/components/ui/label"; import { Input } from "@/components/ui/input"; import { Button } from "@/components/ui/button"; import Image from "next/image"; import Link from "next/link";
 
 export default function Cadastro() {
-    return (
-        <div className="flex justify-start p-17 min-h-screen bg-black">
-            <div className=" text-sky-400">
-                <form className="gap-4 flex flex-col ">
-                    <h1 className="text-white font-bold text-2xl">Realizar Cadastro</h1>
-                    <Label htmlFor="nome">Nome completo</Label>
-                    <Input className="rounded-full" type="text" id="nome" name="nome" placeholder="Digite seu nome completo" required></Input>
-                    <Label htmlFor="email">Email</Label>
-                    <Input className="rounded-full" type="text" id="email" name="email" placeholder="Digite seu email" required></Input>
-                    <Label htmlFor="password">Senha</Label>
-                    <Input className="rounded-full" type="password" id="password" name="password" placeholder="Digite sua senha" required></Input>
-                    <Label htmlFor="password2">Confirme sua senha</Label>
-                    <Input className="rounded-full" type="password" id="password2" name="password2" placeholder="Confirme sua senha" required></Input>
-                    <div>
-                        <Button className="w-48 rounded-full text-black bg-white hover:bg-sky-400"type="submit">Cadastrar</Button>
-                    </div>
-                </form>
-            </div>
-
-        </div>
-
-    );
+  return (
+    <div className="flex min-h-screen">
+      <div className="w-1/2 bg-black flex flex-col justify-center px-20">
+        <form className="flex flex-col gap-3 text-sky-400">
+          <h1 className="text-white font-bold text-center text-2xl">Realizar Cadastro</h1>
+          <p className="text-sm text-white mb-4">Cadastre-se para usar o IVOX, seu assistente pessoal com IA, comandos por voz e respostas personalizadas.</p>
+          <Label htmlFor="nome">Nome completo</Label>
+          <Input id="nome" name="nome" type="text" placeholder="Nome completo" className="rounded-full px-4 py-2 bg-black border border-white text-white" required />
+          <Label htmlFor="email">Email</Label>
+          <Input id="email" name="email" type="email" placeholder="Email" className="rounded-full px-4 py-2 bg-black border border-white text-white" required />
+          <Label htmlFor="password">Senha</Label>
+          <Input id="password" name="password" type="password" placeholder="Senha" className="rounded-full px-4 py-2 bg-black border border-white text-white" required />
+          <Label htmlFor="password2">Confirmar senha</Label>
+          <Input id="password2" name="password2" type="password" placeholder="Confirmar senha" className="rounded-full px-4 py-2 bg-black border border-white text-white" required />
+          <p className="text-white text-sm mt-2">Já tem conta? <Link href="/login" className="text-sky-400 underline">clique aqui</Link></p>
+          <Button type="submit" className="bg-white text-black rounded-full mt-4 w-full hover:bg-sky-400 hover:text-white transition">Cadastrar</Button>
+          <Button type="submit" className="bg-white text-black rounded-full mt-4 w-full hover:bg-sky-400 hover:text-white transition"> <Image src="/images/google-icon-1.png" alt="Google" width={20} height={20} />
+            Criar conta com Google</Button>
+        </form>
+      </div>
+      <div className="w-1/2 bg-gray-200 flex items-center justify-center">
+        <Image src="/images/ivox-2.jpg" alt="Logo IVOX" width={1000} height={1000} />
+      </div>
+    </div>
+  );
 }
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,26 +1,31 @@
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Login() {
     return (
-        <div className="min-h-screen flex justify-center bg-gray-700">
-            <div className="">
-                <form className="flex flex-col gap-4 mt-31 text-white">
-                    <h1 className="flex justify-center text-2xl font-bold text-white">Login</h1>
-                    <Label htmlFor="email">Email</Label>
-                    <Input className="rounded-full" type="email" id="email" name="email" placeholder="Digite seu email" required></Input>
-                    <Label htmlFor="password">Senha</Label>
-                    <Input className="rounded-full" type="password" id="password" name="password" placeholder="Digite sua senha" required></Input>
-                    <div>
-                        <p className="">Nao possui uma conta?<a className="text-blue-700" href="#"> clique aqui</a></p>
-                    </div>
-                    <div className="">
-                        <Button type="submit" className=" rounded-full font-bold w-60 bg-black hover:bg-blue-700">Acessar</Button>
-                    </div>
+        <div className="min-h-screen flex">
+            <div className="w-1/2 flex items-center justify-center bg-gray-200 rounded-l-2xl">
+                <Image src={"/images/ivox.jpg"} alt="Logo Ivox" width={1000} height={1000} />
+            </div>
+            <div className="w-1/2 bg-black text-white flex items-center justify-center rounded-r-2xl">
+                <form className="w-[80%] max-w-md flex flex-col gap-4">
+                    <h1 className="text-3xl font-bold text-center">Login</h1>
+                    <p className="text-center text-sm text-white">Se você já possui uma conta, pode fazer login com seu email e senha. </p>
+                    <Label className="text-sky-400" htmlFor="email">Email</Label>
+                    <Input className="rounded-full text-white" type="email" id="email" placeholder="Digite seu email" />
+                    <Label className="text-sky-400" htmlFor="password">Senha</Label>
+                    <Input className="rounded-full text-white" type="password" id="password" placeholder="Digite sua senha" />
+                    <Button className="rounded-full bg-white text-black font-bold hover:bg-sky-400">Entrar</Button>
+                    <Button className="rounded-full flex gap-2 justify-center items-center bg-white text-black hover:bg-sky-400">
+                        <Image src="/images/google-icon-1.png" alt="Google" width={20} height={20} />
+                        Entrar com o Google
+                    </Button>
+                    <p className="text-white text-sm mt-2">Nao tem conta? <Link href="/cadastro" className="text-sky-400 underline">clique aqui</Link></p>
                 </form>
             </div>
         </div>
-
     );
 }
